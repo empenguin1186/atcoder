c598c9e6724022700f84453b96247a6d724f9c68
empenguin1186 <emperorpenguint1186@gmail.com>
2022-01-28 23:44:00 +0900

    :+1: 演習追加

--- src/jp/co/empenguin/atcoder/Template.java
+++ src/jp/co/empenguin/atcoder/Template.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Template {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         scan.close();
     }
 }
--- src/jp/co/empenguin/atcoder/utils/Utils.java
+++ src/jp/co/empenguin/atcoder/utils/Utils.java
@@ -1,6 +1,7 @@
 package jp.co.empenguin.atcoder.utils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -76,5 +77,41 @@ public class Utils {
             }
         }
         return result;
+    }
+
+    /**
+     * 順列全探索の組み合わせを列挙する関数
+     * @param array 対象の配列(一番最初に呼ばれる際には昇順にソート済であること)
+     * @return 次の組み合わせが存在するかどうか
+     */
+    public static boolean nextPermutation(int[] array) {
+
+        int start = 0;
+        int end = array.length;
+
+        for(int i = end-2; i >= start; i--) {
+            // array[i] < array[i+1] となる最大の i を特定. i+1 以降は降順に並んでいることになる.
+            if(array[i] < array[i+1]) {
+
+                // 末尾から array[i] < array[j] となる j を特定し、array[i] と array[j] を入れ替える
+                // i+1 以降は降順で並んでいることから array[j-1] <= array[i] < array[j] < array[j-1], array[j-2], ...となり、
+                // array[j] は array[i] より大きいかつ array[i] との差分が最小となる.
+                // これにより辞書順的に現在の配列の並びの次にくるパターンを導出することができる.
+                int j = end - 1;
+                while(array[i] >= array[j]) {
+                    j--;
+                }
+                int temp = array[j];
+                array[j] = array[i];
+                array[i] = temp;
+
+                // なるべく辞書順的に上位に来るように、i+1 以降は昇順にソートする
+                Arrays.sort(array, i+1, end);
+
+                return true;
+            }
+        }
+
+        return false;
     }
 }
