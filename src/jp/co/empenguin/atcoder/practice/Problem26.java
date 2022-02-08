7fc9ec5a78065732c01878afc8a7f10a27b75226
empenguin1186 <emperorpenguint1186@gmail.com>
2022-02-08 23:44:23 +0900

    :+1: 回答追加

--- src/jp/co/empenguin/atcoder/practice/Problem26.java
+++ src/jp/co/empenguin/atcoder/practice/Problem26.java
@@ -6,6 +6,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
+/**
+ * TODO 間違いあり
+ */
 public class Problem26 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
