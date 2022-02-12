8d9e640ef496d28957dc844e0aa213786908f26f
empenguin1186 <emperorpenguint1186@gmail.com>
2022-02-12 13:36:23 +0900

    :+1: 解法追加

---
+++ src/jp/co/empenguin/atcoder/practice/Problem36.java
@@ -0,0 +1,64 @@
+package jp.co.empenguin.atcoder.practice;
+
+import java.util.Scanner;
+
+/**
+ * TODO TLE　発生
+ */
+public class Problem36 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+        int W = scan.nextInt();
+        int[][] w = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            w[i][0] = scan.nextInt();
+            w[i][1] = scan.nextInt();
+        }
+
+        int[][] dp = new int[W+1][N];
+        for (int i = 0; i < W+1; i++) {
+            for (int j = 0; j < N; j++) {
+                int vi = w[j][0];
+                int wi = w[j][1];
+
+                // 重さの上限が0の場合
+                if (i == 0) {
+                    dp[i][j] = 0;
+                    break;
+                }
+
+                // 初めの要素のみを考慮する場合
+                if (j == 0) {
+                    if (wi <= i) {
+                        int numOfItems = i / wi;
+                        dp[i][j] = vi * numOfItems;
+                    } else {
+                        dp[i][j] = 0;
+                    }
+                    continue;
+                }
+
+                // それ以外の場合
+                int b = dp[i-1][j];
+                int c = dp[i][j-1];
+                if (i - wi >= 0) {
+                    int count = 1;
+                    int result = 0;
+                    while (i - wi * count >= 0) {
+                        result = Math.max(result, dp[i - wi * count][j-1] + vi * count);
+                        count++;
+                    }
+                    int d = Math.max(result, b);
+                    dp[i][j] = Math.max(c, d);
+                } else {
+                    dp[i][j] = Math.max(b, c);
+                }
+            }
+        }
+
+        System.out.println(dp[W][N - 1]);
+        scan.close();
+    }
+}
