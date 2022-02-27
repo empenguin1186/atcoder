0f475b91eaf89b9cec4aeb7fc905e7f529add5f0
empenguin1186 <emperorpenguint1186@gmail.com>
2022-02-27 11:25:42 +0900

    :+1: 回答追加

--- src/jp/co/empenguin/atcoder/work20220226/ProblemD.java
+++ src/jp/co/empenguin/atcoder/work20220226/ProblemD.java
@@ -1,53 +1,54 @@
 package jp.co.empenguin.atcoder.work20220226;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class ProblemD {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int Q = scan.nextInt();
-        List<Long> list = new ArrayList<>();
+        TreeMap<Long, Integer> treeMap = new TreeMap<>();
+
         for (int i = 0; i < Q; i++) {
             int operation = scan.nextInt();
+            long x = scan.nextLong();
             if (operation == 1) {
-                list.add(scan.nextLong());
-                Collections.sort(list);
+                if (treeMap.containsKey(x)) {
+                    treeMap.put(x, treeMap.get(x) + 1);
+                } else {
+                    treeMap.put(x, 1);
+                }
                 continue;
             }
 
-            long x = scan.nextLong();
             int k = scan.nextInt();
+            Long ans = x;
+            int count = treeMap.getOrDefault(x, 0);
             if (operation == 2) {
-                int index = upperBound(list, x);
-                int nextIndex = index - k + 1;
-                if (nextIndex >= 0) {
-                    System.out.println(list.get(nextIndex));
-                } else {
-                    System.out.println(-1);
+                while (count < k) {
+                    ans = treeMap.lowerKey(ans);
+                    if (ans == null) {
+                        break;
+                    } else {
+                        count += treeMap.get(ans);
+                    }
                 }
             } else {
-                int index = lowerBound(list, x);
-                int nextIndex = index + k - 1;
-                if (nextIndex < list.size()) {
-                    System.out.println(list.get(nextIndex));
-                } else {
-                    System.out.println(-1);
+                while (count < k) {
+                    ans = treeMap.higherKey(ans);
+                    if (ans == null) {
+                        break;
+                    } else {
+                        count += treeMap.get(ans);
+                    }
                 }
+            }
+            if (ans != null) {
+                System.out.println(ans);
+            } else {
+                System.out.println(-1);
             }
         }
         scan.close();
-    }
-
-    public static int upperBound(List<Long> list, long value) {
-        int temp = Collections.binarySearch(list, value);
-        return temp < 0 ? Math.min(-1 * temp - 1, list.size() - 1) : temp;
-    }
-
-    public static int lowerBound(List<Long> list, long value) {
-        int temp = Collections.binarySearch(list, value);
-        return temp < 0 ? -1 * temp - 1 : temp;
     }
 }
